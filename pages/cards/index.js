e7e7751ec856f53f5799c97ff0ce65302e560e74
Phillip Conrad <pconrad@Phillips-MacBook-Pro.local>
2020-04-28 11:46:23 -0700

    pc - change page1 placeholder to link for cards

---
+++ pages/cards/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+function Cards(props) {
+  return (
+    <Layout>
+      <p>This a place holder for the Cards page</p>
+    </Layout>
+  );
+}
+
+export default Cards;
